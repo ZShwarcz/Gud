c8483b0c222d955903f71b6f5fef33d55441c94a
ZShwarcz <50367391+ZShwarcz@users.noreply.github.com>
2020-03-12 10:56:49 +0300

    Add files via upload

---
+++ WhiteNoise.py
@@ -0,0 +1,27 @@
+import numpy as np
+from numpy.fft import rfft, rfftfreq
+import matplotlib.pyplot as plt
+
+FD = 10000 # частота дискретизации, отсчётов в секунду
+N = 1000 # длина входного массива, 0.091 секунд при такой частоте дискретизации
+pure_sig = np.array([np.sin(2.*np.pi*50.0*t/FD) for t in range(N)])
+
+spectrum = rfft(pure_sig)
+
+mean = 0
+std = 1
+num_samples = 1000
+samples = np.random.normal(mean, std, size=num_samples)
+
+plt.plot(samples)
+plt.show()
+
+white_noise = pure_sig + samples
+
+plt.plot(np.arange(N)/float(FD), white_noise, 'r')
+plt.xlabel(u'Время, c')
+plt.ylabel(u'Высота')
+plt.title(u'Сигнал 50 Гц')
+plt.grid(True)
+plt.show()
+
